6b96c9564c8fb7b603a71bcad83f542b464751e7
MartinGil <mgil@mobydigital.com>
2021-11-16 19:29:37 -0300

    Se agregaron tres tablas en la bd y se insertaron datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,18 @@ INSERT INTO authorities(username, authority)
 values('user','ROLE_USER');
 INSERT INTO authorities(username, authority)
 values('admin','ROLE_ADMIN');
+
+
+insert into candidatos (nombre,apellido,tipo,numero_documento) values('Martin','Gil','1','39098865');
+insert into candidatos (nombre,apellido,tipo,numero_documento) values('Juan','Perez','1','12345678');
+insert into candidatos (nombre,apellido,tipo,numero_documento) values('Sergio','Aguero','1','12345612');
+
+insert into tecnologias (nombre,version) values ('Java',1.8);
+insert into tecnologias (nombre,version) values ('Java',11);
+insert into tecnologias (nombre,version) values ('Angular',5);
+
+insert into experiencia (candidato,tecnologia,experiencia) values (1,1,3);
+insert into experiencia (candidato,tecnologia,experiencia) values (1,2,3);
+insert into experiencia (candidato,tecnologia,experiencia) values (2,1,2);
+insert into experiencia (candidato,tecnologia,experiencia) values (2,2,1);
+insert into experiencia (candidato,tecnologia,experiencia) values (3,3,4);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,30 @@ create table authorities
 );
 create
 unique index ix_auth_username on authorities (username,authority);
+
+create table `tecnologias`(
+                              id int auto_increment,
+                              nombre varchar(100),
+                              version_tec varchar(10),
+                              constraint pk_tecnologias primary key (id)
+);
+
+create table `candidatos` (
+                              id int auto_increment,
+                              nombre varchar(50),
+                              apellido varchar(50),
+                              tipo_doc varchar(3),
+                              dni varchar(50) not null unique,
+                              nacimiento date,
+                              constraint pk_candidatos primary key (id)
+);
+
+create table `experiencia`(
+                              id int auto_increment,
+                              id_tec int not null,
+                              id_candidato int not null,
+                              experiencia int,
+                              constraint pk_can_tec primary key (id),
+                              constraint fk_tecnologia_candidato foreign key (id_tec) references tecnologias (id),
+                              constraint fk_candidato_tecnologia foreign key (id_candidato) references candidatos (id)
+);
