12fcdd829c692b4d4fd63f70da6d642d874e828a
MartinGil <mgil@mobydigital.com>
2021-11-17 13:51:54 -0300

    Se modifico el permiso de los roles de los endpoints

--- src/main/java/moby/evaluacion/security/SecurityConfig.java
+++ src/main/java/moby/evaluacion/security/SecurityConfig.java
@@ -6,11 +6,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
+
+import static moby.evaluacion.utils.Constants.*;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -34,9 +35,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception{
 
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/").permitAll()
+                .antMatchers("/candidato/cargar-candidato/").hasRole(ROL_ADMIN)
+                .antMatchers("/candidato/eliminar-candidato/").hasRole(ROL_ADMIN)
+                .antMatchers("/candidato/modificar-candidato/").hasRole(ROL_ADMIN)
+                .antMatchers("/candidato/mostrar-candidato/").hasAnyRole(ROL_USER, ROL_ADMIN)
+                .antMatchers("/tecnologia/cargar-tecnologia/").hasRole(ROL_ADMIN)
+                .antMatchers("/tecnologia/eliminar-tecnologia/").hasRole(ROL_ADMIN)
+                .antMatchers("/tecnologia/modificar-tecnologia/").hasRole(ROL_ADMIN)
+                .antMatchers("/tecnologia/mostrar-tecnologia/").hasAnyRole(ROL_USER,ROL_ADMIN)
+                .antMatchers("/exp/cargar-tecnologia/").hasAnyRole( ROL_USER,ROL_ADMIN)
+                .antMatchers("/exp/candidatos-tecnologia/").hasAnyRole(ROL_ADMIN)
                 .and().formLogin();
     }
 }
